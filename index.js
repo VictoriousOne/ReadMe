190af46e5d750fc7ba0b693f59f6b1ab5a41282f
Kerry Long <victory173451@gmail.com>
2022-01-30 20:13:54 -0500

    added prompts

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const genTitle = require('./src/title');
-const genDesc = require('./src/desc');
-const genInstall = require('./src/install');
-const genUsage = require('./src/usage');
-const genContrib = require('./src/contrib');
-const genTest = require('./src/test');
-const genToc = require('./src/Toc');
-const genGitHub  = require('./src/gitHub');
-const genLicense = require('./src/license');
+//const genDesc = require('./src/desc');
+//const genInstall = require('./src/install');
+//const genUsage = require('./src/usage');
+//const genContrib = require('./src/contrib');
+//const genTest = require('./src/test');
+//const genToc = require('./src/Toc');
+//const genGitHub  = require('./src/gitHub');
+//const genLicense = require('./src/license');
 
 //onst pageTitle = genTitle(title);
 
@@ -22,3 +22,52 @@ const pageTitle = genTitle(title);
 //    if (err) throw err;
 //    console.log("Title written to README file");
 //})
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for your project.'
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'Enter installation instructions.'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage instructions.'
+      },
+      {
+        type: 'input',
+        name: 'contrib',
+        message: 'Enter contribution guidelines.'
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Enter testing instructions.'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your github user name.'
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose your license type.',
+        choices : ["MIT", "GNU", "none"]
+      }
+      
+    ]);
+  };
+
+  promptUser();
