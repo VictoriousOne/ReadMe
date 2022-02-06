06866d43452fedee5f5c44ec5d0ab44cf60a2a7e
Kerry Long <victory173451@gmail.com>
2022-02-05 19:29:04 -0500

    added validate for data prompts

---
+++ README.md
@@ -0,0 +1,35 @@
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+# ReadMe Generator
+
+## Description
+Generate ReadME file for Github
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributions](#contributions)
+* [License](#license)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+Clone repository, Install Node, Initialize NPM, Install Require Package
+
+## Usage
+node appName
+
+## Contributions
+Contributions welcome
+
+## License
+The product is licensed under MIT
+
+## Tests
+Validate all data propmts
+
+## Questions
+Github Profile: https://github.com/GittyUp
+
+For more questions, email 123@gmail.com
--- index.js
+++ index.js
@@ -18,48 +18,120 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'title',
-        message: 'What is the title of your project?'
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+          if (titleInput) {
+            return true;
+          } else {
+            console.log('Please enter a title for your project!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Enter a description for your project.'
+        message: 'Enter a description for your project. (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter a description for your project!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'install',
-        message: 'Enter installation instructions.'
+        message: 'Enter installation instructions. (required)',
+        validate: installInput => {
+          if (installInput) {
+            return true;
+          } else {
+            console.log('Please enter installation instructions for your project!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'usage',
-        message: 'Enter usage instructions.'
+        message: 'Enter usage instructions. (required)',
+        validate: usageInput => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log('Please enter usage instructions for your project!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'contrib',
-        message: 'Enter contribution guidelines.'
+        message: 'Enter contribution guidelines. (Required)',
+        validate: contribInput => {
+          if (contribInput) {
+            return true;
+          } else {
+            console.log('Please enter contribution guidelines for your project!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'tests',
-        message: 'Enter testing instructions.'
+        message: 'Enter testing instructions. (Required)',
+        validate: testInput => {
+          if (testInput) {
+            return true;
+          } else {
+            console.log('Please enter testing instructions for your project!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your github user name.'
+        message: 'Enter your github user name. (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your github user name for your project!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'email',
-        message: 'Enter youur email address.'
+        message: 'Enter youur email address. (Required)',
+        validate: emailInput => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log('Please enter your email address for your project!');
+            return false;
+          }
+        }
       },
       {
         type: 'list',
         name: 'license',
-        message: 'Choose your license type.',
-        choices : ["MIT", "GNU", "none"]
+        message: 'Choose your license type. (Required)',
+        choices : ["MIT", "GNU", "none"],
+        validate: licenseInput => {
+          if (licenseInput) {
+            return true;
+          } else {
+            console.log('Please select a license for your project!');
+            return false;
+          }
+        }
       }
       
     ]);
